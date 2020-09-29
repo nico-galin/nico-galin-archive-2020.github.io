a8bd1ca27ea06fcc71cc82eccc68b0f93c037e24
Nicholas Galin <nico.galin@gmail.com>
2020-09-29 13:03:20 -0700

    initialization and pages fixes

--- src/App.js
+++ src/App.js
@@ -1,58 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
-import { Counter } from './components/counter/Counter';
-import './App.css';
+import { Tile } from './components/tile/Tile';
+import { SkewedHeader } from './components/skewedHeader/Skewedheader';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <SkewedHeader title={"nico galin."} desc={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "}/>
+      <img src={logo} className="App-logo" alt="logo" />
+      <a>[Site in Progress]</a>
+      <Tile title="graphic design" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." externalUrl="https://www.instagram.com/noir.graphics/"/>
+    </header>
+  </div>
+);
 
 export default App;
--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,59 +1,19 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import {
-  decrement,
-  increment,
-  incrementByAmount,
-  incrementAsync,
-  selectCount,
-} from './tileSlice';
-import styles from './Tile.module.css';
+import React from 'react';
+import { ButtonToolbar } from '../buttonToolbar/ButtonToolbar';
+import styles from './Tile.module.scss';
+import Icon from '@material-ui/core/Icon';
 
-export function Counter() {
-  const count = useSelector(selectCount);
-  const dispatch = useDispatch();
-  const [incrementAmount, setIncrementAmount] = useState('2');
-
+export function Tile(props) {
   return (
     <div>
-      <div className={styles.row}>
-        <button
-          className={styles.button}
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          +
-        </button>
-        <span className={styles.value}>{count}</span>
-        <button
-          className={styles.button}
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          -
-        </button>
-      </div>
-      <div className={styles.row}>
-        <input
-          className={styles.textbox}
-          aria-label="Set increment amount"
-          value={incrementAmount}
-          onChange={e => setIncrementAmount(e.target.value)}
-        />
-        <button
-          className={styles.button}
-          onClick={() =>
-            dispatch(incrementByAmount(Number(incrementAmount) || 0))
-          }
-        >
-          Add Amount
-        </button>
-        <button
-          className={styles.asyncButton}
-          onClick={() => dispatch(incrementAsync(Number(incrementAmount) || 0))}
-        >
-          Add Async
-        </button>
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <h2>{props.title}</h2>
+          <a className={styles.desc}>{props.description}</a>
+        </div>
+        <ButtonToolbar items={["more"]}  primary={0} 
+            icons={[<Icon style={{color:'white'}}>share</Icon>]}
+          />
       </div>
     </div>
   );
